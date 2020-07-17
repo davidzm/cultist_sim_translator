fce5b282e9d41982800fb328798c12d64b427302
David Zuniga <dazoram@gmail.com>
2020-07-17 00:12:18 -0300

    switched

--- app/client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import LanguagesList from './LanguagesList'
-import LanguagesInsert from './LanguagesInsert'
-import LanguagesUpdate from './LanguagesUpdate'
-
-export { LanguagesList, LanguagesInsert, LanguagesUpdate }
